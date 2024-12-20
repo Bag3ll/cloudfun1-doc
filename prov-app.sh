cddd94db4b87a5b8577f4e538647b95acbca6d3b
Jack Britton <157018862+Bag3ll@users.noreply.github.com>
2024-12-20 12:40:21 +0000

    Update prov-app.sh

--- prov-app.sh
+++ prov-app.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# # making the script idempotent
+# rm -rf sparta-app
+
 # update
 sudo apt update -y
 
@@ -10,7 +13,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get install nginx -y
 
 # create the reverse proxy
-sudo sed -i '/location \/ {/a \           proxy_pass http://localhost:3000;' /etc/nginx/sites-available/default
+sudo sed -i 's|try_files.*;|proxy_pass http://localhost:3000/;|' /etc/nginx/sites-available/default
 
 # restart nginx
 sudo systemctl restart nginx
@@ -27,41 +30,40 @@ sudo DEBIAN_FRONTEND=noninteractive bash setup_nodejs.sh
 # install nodejs
 sudo DEBIAN_FRONTEND=noninteractive apt install nodejs -y
 
-# # install unzip
-# sudo DEBIAN_FRONTEND=noninteractive apt install unzip -y
-
 # download sparta-app from github
-git clone https://github.com/Bag3ll/sparta-app.git
-
-# # unzip folder
-# unzip nodejs20-sparta-test-app.zip
+git clone https://github.com/Bag3ll/sparta-app.git sparta-app
 
 # set enviroment variables
 export DB_HOST=mongodb://10.0.3.4:27017/posts
 
 # make enviroment variable persist through restart
-echo 'export DB_HOST=mongodb://10.0.3.4:27017/posts' >> ~/.bashrc
+# echo 'export DB_HOST=mongodb://10.0.3.4:27017/posts' >> ~/.bashrc
 
-# cd into sparta-app
-cd sparta-app
+# exclude everything below from user data when using VM to create image
 
-# go into app folder
-cd app
+# Install pm2 process manager
+sudo npm install -g pm2
+
+# cd into app folder
+cd sparta-app/app
 
 # install app
 npm install
 
-# populate database
+# populate database (npm install does also do this)
 node seeds/seed.js
 
-# start app
-npm start &
+# stop all apps
+pm2 kill
 
-# show that the app is running
-echo "Checking if the app is running..."
-sleep 5
-if curl -s http://localhost:3000 > /dev/null; then
-    echo "App is running successfully on port 3000!"
-else
-    echo "Failed to start the app. Check logs for more details."
-fi
+# start app
+pm2 start app.js
+
+# # show that the app is running
+# echo "Checking if the app is running..."
+# sleep 5
+# if curl -s http://localhost:3000 > /dev/null; then
+#     echo "App is running successfully on port 3000!"
+# else
+#     echo "Failed to start the app. Check logs for more details."
+# fi
